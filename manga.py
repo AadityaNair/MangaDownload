5f7c40e4b33ca421aa2e0b2b6abcae50f1e0222b
Aaditya M Nair <aadityanair6494@gmail.com>
2014-07-06 01:25:36 +0530

    Combined into one file.

--- manga.py
+++ manga.py
@@ -6,25 +6,35 @@
 
     Requires BeautifulSoup4
 """
+#------------------Data Constants--------------------#
+site            = 'http://www.mangapanda.com'
+manga_name      = None
+target_location = '.'
+chapter_range   = {}
 
-__builtins__.Data={
-        'site'            : 'http://www.mangapanda.com',
-        'manga_name'      : None,
-        'target_location' : '.',
-        'chapter_range'   : {}
-        }
-import os, argparse 
-import requisites
+
+#--------------------Imports------------------------#
+import os, argparse,urllib2
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    try:
+        from BeautifulSoup import BeautifulSoup
+    except ImportError:
+        print "Please Install BeautifulSoup4 and try again"
+        exit(-1)
+
+#------------------------Main Integrator Function--------------------------#
 
 def main_function():
-    if not os.path.exists( Data['manga_name'] ):
-        os.mkdir( Data['manga_name'] )
-    os.chdir( Data['manga_name'] )
+    if not os.path.exists( manga_name ):
+        os.mkdir( manga_name )
+    os.chdir( manga_name )
 
-    isNumeric=requisites.check_numeric_chapters()
+    isNumeric=check_numeric_chapters()
 
-    name_list=requisites.get_chapters( Data['chapter_range'], numeric=isNumeric )
-    chapter=Data['chapter_range']['begin']
+    name_list=get_chapters( chapter_range, numeric=isNumeric )
+    chapter=chapter_range['begin']
     
     for chapter_name in name_list:
         if not os.path.exists( str(chapter_name) ):
@@ -34,7 +44,7 @@ def main_function():
         page=1
         nop=-1
 
-        download_url= Data['site'] + '/' + Data['manga_name'] + '/' + str(chapter) + '/'
+        download_url= site + '/' + manga_name + '/' + str(chapter) + '/'
         print str(chapter_name)
 
         while page <= nop or not getPages: 
@@ -46,12 +56,16 @@ def main_function():
                 continue
 
             url=download_url + str(page)
-            obj=requisites.WebResponse(url)
+            obj=WebResponse(url)
 
             if not getPages and not obj.ErrorCode:
-                nop=requisites.get_number_of_pages( obj.page )
+                nop=get_number_of_pages( obj.page )
                 getPages=True
-            obj.save_image( str(page) )
+            if not obj.ErrorCode:
+                obj.save_image( str(page) )
+            else:
+                print "Problem"
+                
             if obj.isSaved:
                 print "\t Downloaded."
             else:
@@ -93,13 +107,13 @@ def parse_arguments():
         print '--chapter cannot be specified with --begin/--end. \n'
         parser.parse_args('--help'.split())
     else:
-        Data['manga_name']=args.manga_name
-        Data['target_location']=args.target
+        manga_name=args.manga_name
+        target_location=args.target
         if args.chapter:
-            Data['chapter_range']['begin']=Data['chapter_range']['end']=args.chapter
+            chapter_range['begin']=chapter_range['end']=args.chapter
         else:
-            Data['chapter_range']['begin']=args.begin
-            Data['chapter_range']['end']=args.end
+            chapter_range['begin']=args.begin
+            chapter_range['end']=args.end
 
 
 
@@ -107,7 +121,164 @@ if __name__=='__main__':
     parse_arguments()
 
     current_location=os.path.abspath( os.curdir )
-    os.chdir( os.path.abspath( Data['target_location'] ) )
+    os.chdir( os.path.abspath( target_location ) )
 
     main_function()
     os.chdir( current_location )
+
+class WebResponse(object):
+    def __init__(self,url):
+        self.url=url
+        self.page=''
+        self.ErrorCode=0
+
+        try:
+            headers = { 'User-Agent' : 'Mozilla/5.0' }
+            req = urllib2.Request(url, None, headers)
+            response=urllib2.urlopen(req,timeout=10)
+        except urllib2.URLError:
+            self.ErrorCode=2  # Network Unreachable
+        except urllib2.socket.timeout:
+            self.ErrorCode=1  # Connection too slow
+        else:
+            self.page=response.read()
+
+def save_image(web_page,name):
+    if not web_page.ErrorCode:
+        page=BeautifulSoup(web_page.page)
+        url=page.body.img['src']
+    
+        image=WebResponse(url)
+        if not image.ErrorCode:
+            extn=url[url.rfind('.')+1:]
+            new_name= str(name)+extn
+            
+            f=open(new_name,'wb')
+            f.write(image.page)
+            f.close()
+
+#--------------------------------Functions to generate Metadata-------------------------------#
+
+def get_number_of_pages(response):
+    soup=BeautifulSoup(response)
+
+    l=soup.body.find(id='pageMenu').children
+    page_count=len(list(l))/2
+    return page_count 
+
+def get_chapters( chapter_range, numeric): 
+    begin=1
+    if chapter_range.has_key('begin'):
+        begin=chapter_range['begin']
+    if chapter_range.has_key('end'):
+        end=chapter_range['end']
+    chapter=begin
+
+    if numeric:
+        if chapter_range.has_key('end'):
+            return range(begin,end+1)
+        else:
+            return InfiniteSequence(begin)
+    
+    chapter_list_location=get_list_location( Data['manga_name'] )
+    isError=False
+    try:
+        response=WebResponse( chapter_list_location )
+    except ValueError:
+        isError=True
+    else: 
+        if response.ErrorCode:
+            isError=True
+        else:
+            soup=BeautifulSoup(response.page)
+            l=soup.body.find_all('tr')
+            if not chapter_range.has_key('end'):
+                end=len(l)
+
+            return_list=[]
+    if isError:
+        print 'Unable to download chapter names.Going with numbers'
+        return range(begin,end+1)
+    
+    while chapter >= begin and chapter <= end:
+        try:
+            name= l[ 11+chapter ].a.string + l[ 11+chapter ].td.contents[4]
+        except IndexError:
+            break
+        return_list.append(name)
+        chapter=chapter+1
+    return return_list
+
+def get_list_location(manga_name):
+    url=cache_url(get=True)
+    if url is not False:
+        print "Used cached url."
+        return url
+    
+    for i in range(5):
+        response=WebResponse("http://www.mangapanda.com/alphabetical")
+        if not response.ErrorCode:
+            break
+        else:
+            if response.ErrorCode==2:
+                print 'Network Unreachable.Trying again...'
+            else:
+                print 'Connection too slow.Retrying...'
+
+    if response.ErrorCode==2:
+        print 'Network Unreachable.Check your internet or proxy settings and try again.'
+        exit(-2)
+    if response.ErrorCode==1:
+        return 1
+
+    soup=BeautifulSoup(response.page)
+    l=soup.find_all('div',class_='series_alpha')
+    isExist=False
+
+    for target in l:
+        if target.a.string==manga_name[0].upper():
+            break
+    for loc in target.find_all('li'):
+        if loc.a.string.lower()==manga_name.lower():
+            isExist=True
+            break
+    if isExist:    
+        print 'List Location Discovered.'
+        url=Data['site'] + loc.a['href']
+        cache_url(URL=url,get=False)
+        return url
+    else:
+        print 'Manga Name does not exist.Check spelling and try again.'
+        exit(-3)
+
+#-----------------------Other Minor Funtions-----------------------#
+
+def check_numeric_chapters():
+    a=os.walk('.')
+    b=a.next()
+    chapter_list=b[1]
+
+    for chapter in chapter_list:
+        try:
+            int(chapter)
+        except ValueError:
+            return False
+    return True and len(chapter_list)
+
+def cache_url(get,URL=None):
+    if not os.path.exists('list_location') and get:
+        return False
+    f=open('list_location','w+')
+    if get:
+        text=f.read()
+        print text
+        return text
+    else:
+        print >> f, URL
+        return True
+
+def InfiniteSequence(begin):
+    while True:
+        yield begin
+        begin+=1
+
