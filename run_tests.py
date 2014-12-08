e07ea1e14fd9eb90e6cdc5e74a98388ac2dcacb2
Aaditya M Nair <aadityanair6494@gmail.com>
2014-12-08 15:33:31 +0530

    Added Travis Integration

---
+++ run_tests.py
@@ -0,0 +1,13 @@
+"""
+    Manga Downloader
+    Author: Aaditya M Nair (Prometheus)	
+    Created On : Mon 08 Dec 2014 15:31:55 IST
+
+    Aggregation of all tests.
+"""
+
+# Stub to test Travis CI
+
+def stub():
+    return True
+stub()
